be2452930ad7ae149084e39aa040089e03cc4ee7
Marcelo Zanchet <82295942+marzanchet@users.noreply.github.com>
2021-11-11 15:48:27 -0300

    added support to upload images

--- DjangoProject/books/models.py
+++ DjangoProject/books/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from uuid import uuid4
 # Create your models here.
 
+def upload_image_book(instace, filename):
+    return f'{instance.id_book}-{filename}'
+
 
 class Books(models.Model):
     id_book = models.UUIDField(primary_key=True, default=uuid4, editable=False)
@@ -12,4 +15,6 @@ class Books(models.Model):
     pages = models.IntegerField()
     publish_company = models.CharField(max_length=255)
     create_at = models.DateField(auto_now_add=True)
-    
+    image = models.ImageField(upload_to=upload_image_book, blank=True, null=True)
+
+
